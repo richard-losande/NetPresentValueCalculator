60439264f2916d5b17024a6bb5abc05a1067ff3a
richard losande <richardl@sprout.ph>
2020-02-18 13:44:52 +0800

    Initial Commit

---
+++ Controllers/NetPresentationValueController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NpvCalculator.Business.DataTransferObjects;
+using NpvCalculator.Business.Factories;
+using NpvCalculator.Business.Services;
+
+namespace NpvCalculator.Controllers
+{
+    [Route("api/netpresentvalue")]
+    [ApiController]
+    public class NetPresentationValueController : Controller
+    {
+        private readonly INetPresentValueService _netPresentValueService;
+        public NetPresentationValueController(INetPresentValueService eNetPresentValueService)
+        {
+            _netPresentValueService = eNetPresentValueService;
+        }
+
+        [HttpPost("netpresentvalueresult")]
+        public IActionResult ComputeNetPresentValue([FromBody]NetPresentValueInputDto request)
+        {
+             var result = _netPresentValueService.GetNetPresentValue(request);
+             return Ok(result);
+        }
+    }
+}
